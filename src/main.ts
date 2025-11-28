ce988a5d0cddc4193bb45b4b38678c4e0469fa65
Simon PIERRE <Docky44@users.noreply.github.com>
2025-11-28 22:27:28 +0100

    Fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://front-invest-opal.vercel.app/'],
+    origin: true,
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization'
